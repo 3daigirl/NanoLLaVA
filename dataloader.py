9dbd7a8509aebe96816ac5661646353924fb675e
Manpreet Kaur <the3daigirl@gmail.com>
2024-10-12 18:15:28 -0700

    enable distributed training

--- dataloader.py
+++ dataloader.py
@@ -14,12 +14,14 @@ class TextDatasetConfig:
 
 class TextDataloader(Dataset):
 
-    def __init__(self, config):
+    def __init__(self, config: TextDatasetConfig, ddp_rank, total_gpus):
         with open(config.text_file, 'r') as f:
             text = f.read()
 
         self.B = config.batch_size
         self.T = config.block_size
+        self.ddp_rank = ddp_rank
+        self.total_gpus = total_gpus
 
         enc = tiktoken.get_encoding('gpt2')
         self.tokens = torch.tensor(enc.encode(text))
@@ -35,4 +37,34 @@ class TextDataloader(Dataset):
         batch = self.tokens[idx: idx + B * T + 1]
         inputs = batch[:-1].view(B, T)
         targets = batch[1:].view(B, T)
+        return inputs, targets
+
+
+class DataloaderLite:
+    def __init__(self, text_file, B, T, ddp_rank, total_gpus):
+        with open(text_file, 'r') as f:
+            text = f.read()
+
+        self.B = B
+        self.T = T
+        self.ddp_rank = ddp_rank
+        self.total_gpus = total_gpus
+
+        enc = tiktoken.get_encoding('gpt2')
+        self.tokens = torch.tensor(enc.encode(text))
+        print(f"Loaded {len(self.tokens)}")
+        print(f"Number of batches = {len(self.tokens)/(self.B * self.T)}")
+
+        # track data pointer in this dataloader
+        self.current_position = self.ddp_rank * self.B * self.T
+
+
+    def next_batch(self):
+        B, T = self.B, self.T
+        batch = self.tokens[self.current_position: self.current_position + B * T + 1]
+        inputs = batch[:-1].view(B, T)
+        targets = batch[1:].view(B, T)
+        self.current_position += B * T * self.total_gpus
+        if self.current_position + (B * T * self.total_gpus + 1) > len(self.tokens):
+            self.current_position = B * T * self.total_gpus
         return inputs, targets
--- train.py
+++ train.py
@@ -1,34 +1,69 @@
 import time
+import os
 
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+import torch.distributed as dist
+from torch.distributed import init_process_group, destroy_process_group
+from torch.nn.parallel import DistributedDataParallel as DDP
 
 import math
 import tiktoken
 
 from gpt2_model import GPT, GPTConfig
-from dataloader import TextDataloader, TextDatasetConfig
+from dataloader import TextDataloader, DataloaderLite, TextDatasetConfig
 from utils import get_lr
-
-# auto-detect device
-if torch.cuda.is_available():
-    device = 'cuda'
-elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
-    device = 'mps'
-#TODO: uncomment when running on GPU
-device = 'cpu'
-print(f"Using device: {device}")
 
 torch.set_float32_matmul_precision('high')
 
+# DDP multi-GPU training
+ddp = int(os.environ.get('RANK', -1)) != -1
+if ddp:
+    assert torch.cuda.is_available()
+    init_process_group(backend='nccl')
+    ddp_rank = int(os.environ['RANK'])
+    ddp_local_rank = int(os.environ['LOCAL_RANK'])
+    ngpus = int(os.environ['WORLD_SIZE'])
+    device = f'cuda:{ddp_local_rank}'
+    torch.cuda.set_device(device)
+    master_process = ddp_rank == 0
+else:
+    ddp_rank = 0
+    ddp_local_rank = 0
+    ngpus = 1
+    master_process = True
+    # auto-detect device
+    if torch.cuda.is_available():
+        device = 'cuda'
+    elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+        device = 'mps'
+    #TODO: uncomment when running on GPU
+    device = 'cpu'
+
+print(f"Using device: {device}")
+
+total_batch_size = 524288     # Total batch size across all gpus, 0.5 million
+B = 16                  # process B batch-size per gpu
+T = 1024                # sequence length
+assert total_batch_size % (B * T * ngpus) == 0
+num_grad_accum = total_batch_size // (B * T * ngpus)
+if master_process:
+    print("Number of gradient accumulation steps: {}".format(num_grad_accum))
+
+# set seed
+torch.manual_seed(1337)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(1337)
+
 # initialize dataset
-dataset = TextDataloader(TextDatasetConfig(
-    text_file='data/input.txt',
-    # batch_size=16,
-    # block_size=1024
-))
-data_ptr = iter(dataset)
+# dataset = TextDataloader(TextDatasetConfig(
+#     text_file='data/input.txt',
+#     batch_size=B,
+#     block_size=T
+# ), ddp_rank=ddp_rank, total_gpus=ngpus)
+# data_ptr = iter(dataset)
+dataset = DataloaderLite(B, T, 'data/input.txt', ddp_rank, ngpus)
 
 # create randomly initialized model
 model = GPT(GPTConfig(
@@ -38,29 +73,45 @@ model = GPT(GPTConfig(
 model = model.to(device)
 
 ## Compiling the model leads to 57% faster performance
-#TODO: uncomment
-# model = torch.compile(model)
+model = torch.compile(model)
+if ddp:
+    model = DDP(model, device_ids=[ddp_local_rank])
 
 # train model
 # optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4, betas=(0.9, 0.95), eps=1e-8)
 optimizer = model.configure_optimizer(weight_decay=0.1, learning_rate=6e-4, betas=(0.9, 0.95), device=device)
 
 for i in range(50):
-    x, y = next(data_ptr)
-    t0 = time.time()
-    x, y = x.to(device), y.to(device)
     optimizer.zero_grad()
-    #TODO: uncomment while running on GPU
-    # with torch.autocast(device_type=device, dtype=torch.bfloat16):
-    logits, loss = model(x, y)
-    loss.backward()
+    t0 = time.time()
+    batch_loss = 0.0
+    for step in range(num_grad_accum):
+        x, y = dataset.next_batch()
+        x, y = x.to(device), y.to(device)
+        #TODO: uncomment while running on GPU
+        # with torch.autocast(device_type=device, dtype=torch.bfloat16):
+        logits, loss = model(x, y)
+        # Average is taken over gradients of all training examples in the batch. 
+        # Hence divide loss by num_grad_accum to ensure mean of all training examples is taken
+        loss = loss / num_grad_accum
+        batch_loss += loss.detach()
+        if ddp:
+            model.requires_backward_grad_sync = (step == num_grad_accum - 1)
+        loss.backward()
+        if ddp:
+            dist.all_reduce(batch_loss, op=dist.ReduceOp.AVG)
     norm = nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
     # set new learning rate based on cosine decay
     lr = get_lr(i)
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
     optimizer.step()
+    
     #TODO: uncomment while running on GPU
-    # torch.cuda.synchronize()
+    torch.cuda.synchronize()
     t1 = time.time()
-    print("Loss: {}, norm: {}, dt: {}, tps: {}".format(loss.item(), norm, t1-t0, dataset.B*dataset.T/(t1-t0)))
+    tps = B * T * num_grad_accum * ngpus / (t1 - t0)
+    print("Loss: {:6f}, norm: {}, dt: {}, tps: {}".format(batch_loss.item(), norm, t1-t0, tps))
+
+if ddp:
+    destroy_process_group()
